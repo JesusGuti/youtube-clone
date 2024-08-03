bd55725ae9160bc45cc7e12cb7d9cb38669105f9
JesusGuti <jesusgonzales0968@gmail.com>
2024-08-02 23:42:14 -0400

    Started project

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import ThreeLines from "./icons/ThreeLines"
+import YoutubeIcon from "./icons/YoutubeIcon"
+
+function Header () {
+    return (
+        <header className="flex flex-row">
+            <ThreeLines />
+            {/* <YoutubeIcon /> */}
+            <p>Youtube</p>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/icons/ThreeLines.jsx
@@ -0,0 +1,18 @@
+function ThreeLines () {
+    return (
+        <svg 
+            height="24" 
+            viewBox="0 0 24 24" 
+            width="24" 
+            fill="white"
+            focusable="false" 
+            aria-hidden="true"
+        >
+            <path d="M21 6H3V5h18v1zm0 5H3v1h18v-1zm0 6H3v1h18v-1z"
+            >
+            </path>
+        </svg>
+    )
+}
+
+export default ThreeLines
